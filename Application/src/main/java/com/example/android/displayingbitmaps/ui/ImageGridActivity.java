6327e1edef4c1bc34fb5881571a981c29fa9bc0e
Taisuke Oe <oeuia.t@gmail.com>
2016-01-13 00:30:29 +0900

    added DL4J nature

--- Application/src/main/java/com/example/android/displayingbitmaps/ui/ImageGridActivity.java
+++ Application/src/main/java/com/example/android/displayingbitmaps/ui/ImageGridActivity.java
@@ -20,8 +20,28 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
-import com.example.android.displayingbitmaps.BuildConfig;
-import com.example.android.displayingbitmaps.util.Utils;
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.GradientNormalization;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.api.IterationListener;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 /**
  * Simple FragmentActivity to hold the main {@link ImageGridFragment} and not much else.
@@ -29,11 +49,13 @@ import com.example.android.displayingbitmaps.util.Utils;
 public class ImageGridActivity extends FragmentActivity {
     private static final String TAG = "ImageGridActivity";
 
+    private static Logger log = LoggerFactory.getLogger(ImageGridActivity.class);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (BuildConfig.DEBUG) {
-            Utils.enableStrictMode();
-        }
+//        if (BuildConfig.DEBUG) {
+//            Utils.enableStrictMode();
+//        }
         super.onCreate(savedInstanceState);
 
         if (getSupportFragmentManager().findFragmentByTag(TAG) == null) {
@@ -41,5 +63,82 @@ public class ImageGridActivity extends FragmentActivity {
             ft.add(android.R.id.content, new ImageGridFragment(), TAG);
             ft.commit();
         }
+
+        try {
+            trainMLP();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void trainMLP() throws Exception {
+        Nd4j.ENFORCE_NUMERICAL_STABILITY = true;
+        final int numRows = 28;
+        final int numColumns = 28;
+        int outputNum = 10;
+        int numSamples = 10000;
+        int batchSize = 500;
+        int iterations = 10;
+        int seed = 123;
+        int listenerFreq = iterations / 5;
+        int splitTrainNum = (int) (batchSize * .8);
+        DataSet mnist;
+        SplitTestAndTrain trainTest;
+        DataSet trainInput;
+        List<INDArray> testInput = new ArrayList<>();
+        List<INDArray> testLabels = new ArrayList<>();
+
+        log.info("Load data....");
+        DataSetIterator mnistIter = new MnistDataSetIterator(batchSize, numSamples, true);
+
+        log.info("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .iterations(iterations)
+                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+                .learningRate(1e-1f)
+                .momentum(0.5)
+                .momentumAfter(Collections.singletonMap(3, 0.9))
+                .useDropConnect(true)
+                .list(2)
+                .layer(0, new DenseLayer.Builder()
+                        .nIn(numRows * numColumns)
+                        .nOut(1000)
+                        .activation("relu")
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(1, new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nIn(1000)
+                        .nOut(outputNum)
+                        .activation("softmax")
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.setListeners(Arrays.asList((IterationListener) new ScoreIterationListener(listenerFreq)));
+
+        log.info("Train model....");
+        model.setListeners(Arrays.asList((IterationListener) new ScoreIterationListener(listenerFreq)));
+        while (mnistIter.hasNext()) {
+            mnist = mnistIter.next();
+            trainTest = mnist.splitTestAndTrain(splitTrainNum, new Random(seed)); // train set that is the result
+            trainInput = trainTest.getTrain(); // get feature matrix and labels for training
+            testInput.add(trainTest.getTest().getFeatureMatrix());
+            testLabels.add(trainTest.getTest().getLabels());
+            model.fit(trainInput);
+        }
+
+        log.info("Evaluate model....");
+        Evaluation eval = new Evaluation(outputNum);
+        for (int i = 0; i < testInput.size(); i++) {
+            INDArray output = model.output(testInput.get(i));
+            eval.eval(testLabels.get(i), output);
+        }
+
+        log.info(eval.stats());
+        log.info("****************Example finished********************");
     }
 }
